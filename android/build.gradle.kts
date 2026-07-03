33829fd62f5696cdab39d275ff4c3b9ca4ea1d3c
iphisimdebe-coder <iphisimdebe@gmail.com>
2026-07-03 09:26:22 +0200

    hell no hell no dependncies ngaph nangapha

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,9 +2,9 @@ import java.util.Properties
 import java.io.FileInputStream
 
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
+    id("com.android.application") apply false
+    id("kotlin-android") apply false
+    id("dev.flutter.flutter-gradle-plugin") apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
